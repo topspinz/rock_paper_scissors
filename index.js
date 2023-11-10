068f4b76d5095a76e1d39f8f1b0dade986f7fff3
topspinz <matthew.levstek@gmail.com>
2023-11-10 15:09:49 -0500

    adding winning conditions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("hello world")
+let winCondition= {
+    "rock":"scissors",
+    "paper":"rock",
+    "scissors":"paper"
+    
+}
+
+function getComputerChoice () {
+    arr=["rock","paper","scissors"];
+    return arr[(Math.floor(Math.random() * arr.length))];
+}
+
+
+
+function playRound(playerSelection,computerSelection){
+    if (winCondition[playerSelection]==computerSelection){
+        return ("Player Wins");
+    } else if(playerSelection==computerSelection){
+        return ("It is a Tie");
+    } else {
+        return ("Computer Wins")
+    }
+}
+
+const playerSelection="paper";
+const computerSelection= getComputerChoice();
+console.log(`computer selected: ${computerSelection}`);
+console.log(playRound(playerSelection,computerSelection));
