edc994e4061ce1e8c9539f581fa810ccfacfabcc
topspinz <matthew.levstek@gmail.com>
2023-11-10 16:06:29 -0500

    Added rounds to choose winner

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
-let winCondition= {
-    "rock":"scissors",
-    "paper":"rock",
-    "scissors":"paper"
-    
+let winCondition = {
+    "rock": "scissors",
+    "paper": "rock",
+    "scissors": "paper"
+
 }
 
-function getComputerChoice () {
-    arr=["rock","paper","scissors"];
+var player_score=0;
+var computer_score=0;
+
+function getComputerChoice() {
+    arr = ["rock", "paper", "scissors"];
     return arr[(Math.floor(Math.random() * arr.length))];
 }
 
+function playerWin (){
+    return ++player_score;
+}
+function computerWin (){
+    return ++computer_score;
+}
 
+function chooseWinner(player_score,computer_score){
+    if (player_score > computer_score) { return "Grand Winner: Player Wins";}
+    else if (computer_score>player_score) {return "Grand Winner: Computer Wins";}
+    else {return "Grand Winner: Tie";}
+}
 
-function playRound(playerSelection,computerSelection){
-    if (winCondition[playerSelection]==computerSelection){
+function playRound(playerSelection, computerSelection) {
+    if (winCondition[playerSelection] == computerSelection) {
+        playerWin();
         return ("Player Wins");
-    } else if(playerSelection==computerSelection){
+    } else if (playerSelection == computerSelection) {
+        playerWin();
+        computerWin();
         return ("It is a Tie");
     } else {
-        return ("Computer Wins")
+        computerWin();
+        return ("Computer Wins");
     }
 }
 
-const playerSelection="paper";
-const computerSelection= getComputerChoice();
-console.log(`computer selected: ${computerSelection}`);
-console.log(playRound(playerSelection,computerSelection));
+function game() {
+    
+    for(let i=1;i<=5;i++){
+        const playerSelection = prompt("choose your selection");
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection.toLowerCase(),computerSelection));
+        
+    }
+    return console.log(chooseWinner(player_score,computer_score));
+}
+
+game();
